345cdfd8c8eed036033d6ddb47d9e62b6a5c11af
ryan burke <ryanburketv@gmail.com>
2021-02-21 12:26:27 -0500

    building app

---
+++ src/index.js
@@ -0,0 +1,32 @@
+const { app, BrowserWindow } = require('electron')
+
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 300,
+    height: 200,
+    minimizable: false,
+    maximizable: false,
+    icon: __dirname + "/icon/icon.png",
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  win.setMenuBarVisibility(false);
+  win.loadFile('./src/index.html')
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
